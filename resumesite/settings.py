1eb093b664b9795e1faf449c515122546d228d52
popzyyy <spress61@live.com>
2022-12-13 02:05:37 -0600

    f

--- resumesite/settings.py
+++ resumesite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
